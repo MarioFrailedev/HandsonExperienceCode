95d214ef422eaee4367baf7254c4c43cba24288f
Mario Fraile <mariofrailedev@gmail.com>
2019-07-07 19:53:54 -0500

    stand up code

---
+++ src/main/java/com/manidevs/handsonexperiencecode/HandsonexperiencecodeApplication.java
@@ -0,0 +1,13 @@
+package com.manidevs.handsonexperiencecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsonexperiencecodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandsonexperiencecodeApplication.class, args);
+    }
+
+}
